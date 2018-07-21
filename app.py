e7531427b72de558f3212fe83a8fb5181fdcfa58
smp <smpenna3@gmail.com>
2018-07-21 10:12:18 -0400

    updates

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ logger.addHandler(sh)
 
 ######################################################
 ################# PARAMETERS #########################
-sampleRate = 5  # Sampling rate in Hz
+sampleRate = 120  # Sampling rate in Hz
+guiRate = 0.2 # Number of seconds between gui updates
 tempLogRate = 10  # Number of seconds between temperature logs
+depthLogRate = 1 # Number of seconds between depth logs
 
 tempThreshold = 65  # Threshold for temperature to throw fault
 voltThreshold = 0.35  # Threshold for voltage difference to throw fault
@@ -30,17 +32,15 @@ splitTime = 1 # Number of seconds between trips to log
 tempLog = 'temp.csv'
 tempFaultLog = 'tempFault.csv'
 voltFaultLog = 'voltFault.csv'
+depthLog = 'depth.csv'
 ######################################################
 ######################################################
 
 
 # Define function to randomly generate a number to emulate voltage readings
-def readVoltage():
-	return '%.3f'%(np.random.normal(12, 0.25))
-
-# Define function to randomly generate a number to emulate temperature readings
-def readTemp():
-	return '%.3f'%(np.random.normal(60, 4))
+def getValues():
+    # positive 12, negative 12, temp, depth
+	return '%.3f'%(np.random.normal(12, 0.25)), '%.3f'%(np.random.normal(12, 0.25)), '%.3f'%(np.random.normal(12, 0.25)), '%.3f'%(np.random.normal(12, 0.25))
 
 
 #####################################################
@@ -50,12 +50,32 @@ s = BackgroundScheduler(coalescing=True, misfire_grace_time=5, max_instances=1, 
 s.start()
 
 # Define function to log temperature
+global newTemp
+newTemp = 0
 def logTemp():
-	with open(tempLog, 'a') as f:
-		f.write('\n' + str(time.time()) + ',' + str(readTemp()))
+    global newTemp
+    with open(tempLog, 'a') as f:
+        f.write('\n' + str(time.time()) + ',' + str(newTemp))
 
 # Setup ten second timer to log temperature
 s.add_job(logTemp, 'interval', seconds=tempLogRate, id='tempLog')
+
+######################################################
+######################################################
+
+
+#####################################################
+################ DEPTH LOG #####################
+# Define function to log depth
+global depth
+depth = 0
+def logDepth():
+    global depth
+    with open(depthLog, 'a') as f:
+        f.write('\n' + str(time.time()) + ',' + str(depth))
+
+# Setup ten second timer to log temperature
+s.add_job(logDepth, 'interval', seconds=depthLogRate, id='depthLog')
 
 ######################################################
 ######################################################
@@ -72,11 +92,13 @@ global voltFault
 global tempFault
 global voltTimer
 global tempTimer
+global guiTimer
 
 voltFault = 0
 tempFault = 0
 voltTimer = 0
 tempTimer = 0
+guiTimer = 0
 
 # Define function to reset the fault counts
 def refresh():
@@ -101,14 +123,16 @@ root.title('Monitor') # Set a title for the window
 # Setup the elements in the GUI
 v = tk.Label(root, text='Voltage: ', font="Times 35 bold", padx=10, pady=10)
 v.grid(row=0, column=0, sticky=N+S+E+W)
-voltage = tk.Label(root, text=readVoltage(), font="Times 20")
-voltage.grid(row=0, column=1, sticky=N+S+E+W, columnspan=2)
+voltageP = tk.Label(root, text='0', font="Times 20") # Positive voltage
+voltageP.grid(row=0, column=1, sticky=N+S+E+W)
+voltageN = tk.Label(root, text='0', font="Times 20") # Negative voltage
+voltageN.grid(row=0, column=2, sticky=N+S+E+W)
 voltF = tk.Label(root, text='Voltage Good', font='Times 20', padx=10, pady=40, bg='green')
 voltF.grid(row=2, column=0, sticky=N+S+E+W)
 
 t = tk.Label(root, text='Temp (F): ', font="Times 35 bold", padx=10, pady=10)
 t.grid(row=1, column=0, sticky=N+S+E+W)
-temp = tk.Label(root, text=readTemp(), font="Times 20")
+temp = tk.Label(root, text='0', font="Times 20")
 temp.grid(row=1, column=1, sticky=N+S+E+W, columnspan=2)
 tempF = tk.Label(root, text='Temp Good  ', font='Times 20', padx=10, pady=40, bg='green')
 tempF.grid(row=2, column=1, sticky=N+S+E+W)
@@ -119,46 +143,63 @@ refresh.grid(row=2, column=2, sticky=N+S+E+W)
 # Define function to update GUI
 def updateGUI():
 	# Grab global variables
-	global tempFault, tempTimer
-	global voltFault, voltTimer
+    global tempFault, tempTimer
+    global voltFault, voltTimer
+    global guiTimer
+    global newTemp
+    global depth
 
 	# Read new sensor values
-	newVolt = readVoltage()
-	newTemp = readTemp()
+    p12, n12, newTemp, depth = getValues()
 	
-	# Update the GUI
-	voltage.config(text=newVolt) # Update the voltage
-	temp.config(text=newTemp) # Update the temp
+	# Update the GUI at the set rate
+    if((time.time() - guiTimer) > guiRate):  
+        voltageP.config(text=p12) # Update the positive voltage
+        voltageN.config(text=n12) # Update the negative voltage
+        temp.config(text=newTemp) # Update the temp
+        guiTimer = time.time()
 
 	# Check for faults
-	faultFlag = False
-	if(float(newTemp) > tempThreshold):
-		faultFlag = True
-		temp.config(bg='red')
+    faultFlag = False
+    if(float(newTemp) > tempThreshold):
+        faultFlag = True
+        temp.config(bg='red')
 		
-		if((time.time() - tempTimer) > splitTime):
-			logger.warning('TEMP FAULT #' + str(tempFault))
-			tempTimer = time.time()
-			tempFault = tempFault + 1
-			tempF.config(bg='red', text='Temp Fault x'+str(tempFault))
-	else:
-		temp.config(bg='white')
-
-	if(abs(float(newVolt) - 12) > voltThreshold):
-		faultFlag = True
-		voltage.config(bg='red')
+        if((time.time() - tempTimer) > splitTime):
+            logger.warning('TEMP FAULT #' + str(tempFault))
+            tempTimer = time.time()
+            tempFault = tempFault + 1
+            tempF.config(bg='red', text='Temp Fault x'+str(tempFault))
+    else:
+        temp.config(bg='white')
+    
+    if(abs(float(p12) - 12) > voltThreshold):
+        faultFlag = True
+        voltageP.config(bg='red')
 		
-		if((time.time() - voltTimer) > splitTime):
-			logger.warning('VOLT FAULT #' + str(voltFault))
-			voltTimer = time.time()
-			voltFault = voltFault + 1
-			voltF.config(bg='red', text='Voltage Fault x'+str(voltFault))
-	else:
-		voltage.config(bg='white')
+        if((time.time() - voltTimer) > splitTime):
+            logger.warning('VOLT FAULT #' + str(voltFault))
+            voltTimer = time.time()
+            voltFault = voltFault + 1
+            voltF.config(bg='red', text='Voltage Fault x'+str(voltFault))
+    else:
+        voltageP.config(bg='white')
+        
+    if(abs(float(n12) - 12) > voltThreshold):
+        faultFlag = True
+        voltageN.config(bg='red')
+		
+        if((time.time() - voltTimer) > splitTime):
+            logger.warning('VOLT FAULT #' + str(voltFault))
+            voltTimer = time.time()
+            voltFault = voltFault + 1
+            voltF.config(bg='red', text='Voltage Fault x'+str(voltFault))
+    else:
+        voltageN.config(bg='white')
 	
-	root.update()
+    root.update()
 	
-	root.after(int(1000/int(sampleRate)), updateGUI) # Set to update itself
+    root.after(int(1000/int(sampleRate)), updateGUI) # Set to update itself
 
 # Update for the first time
 updateGUI()
--- arduinoMonitor/arduinoMonitor.ino
+++ arduinoMonitor/arduinoMonitor.ino
@@ -8,19 +8,29 @@ String readString;
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
 // Pin declarations
-P12 = A2 // Positive 12V Rail
-N12 = A1 // Negative 12V Rail
-depth = A0 // Depth sensor
+int P12 = A2; // Positive 12V Rail
+int N12 = A1; // Negative 12V Rail
+int depth = A0; // Depth sensor
+
+// Conversion factor from ADC to voltage
+float convertFactor = 5.0/1023.0;
+
+// Amplify from the voltage division of 12->4
+float voltageDivisionScale = 3;
+
+// Depth sensor is 60 meters per volt
+float depthConvertFactor = 60;
 
 void setup()
 {
   Serial.begin(9600);  // initialize serial communications at 9600 bps
 
   // Setup the pins as output
-  pinMode(P12, OUTPUT);
-  pinMode(N12, OUTPUT);
-  pinMode(depth, OUTPUT);
+  pinMode(P12, INPUT);
+  pinMode(N12, INPUT);
+  pinMode(depth, INPUT);
 
+  tempsensor.wake(); // Wake the sensor
 }
 
 void loop()
@@ -40,7 +50,31 @@ void loop()
   {
     // Check if the "data" command was received, if so send the data
     if(readString == "data"){
+      // Get the temperature reading
+      float c = tempsensor.readTempC();
+      float f = c * 9.0 / 5.0 + 32;
+
+      // Get the positive 12V reading
+      float p12Reading = analogRead(P12) * convertFactor * voltageDivisionScale;
+
+      // Get the negative 12V reading
+      float n12Reading = analogRead(N12) * convertFactor * voltageDivisionScale * -1.0;
+
+      // Get the depth sensor
+      float depthReading = analogRead(depth) * convertFactor * depthConvertFactor;
+
+
+      // Create the JSON string to send out
+      String p12Str = String(p12Reading);
+      String n12Str = String(n12Reading);
+      String depthStr = String(depthReading);
+      String tempStr = String(f);
       
+      String outData = String("{'temp':'" + tempStr + "', 'depth':'" + depthStr + "', 'n12':'" + n12Str + "', 'p12':'" + p12Str + "'}");
+
+      Serial.println(outData);
+
+      readString = "";
     }
   }
 }
