1f57b2c411460bdd9078e7b59e38f1f06bb85244
smp <smpenna3@gmail.com>
2018-07-21 14:38:20 -0400

    u

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def getValues():
             
     try:
         logger.debug("before: " + dataIn)
-        dataIn.replace('$', '"')
+        dataIn = dataIn.replace('$', '"')
         logger.debug("after: " + dataIn)
         dataJSON = json.loads(str(dataIn))
         logger.debug(dataJSON)
