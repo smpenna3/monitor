5a4b5d34758250a8c9ee18dcf7d0a710f103cee5
smp <smpenna3@gmail.com>
2018-07-21 14:55:55 -0400

    update

--- arduinoMonitor/arduinoMonitor.ino
+++ arduinoMonitor/arduinoMonitor.ino
@@ -72,7 +72,7 @@ void loop()
       String tempStr = String(f);
 
       // " is replaced with $ for string formatting, changed later in python
-      String outData = String("'{$temp$:$" + tempStr + "$, $depth$:$" + depthStr + "$, $n12$:$" + n12Str + "$, $p12$:$" + p12Str + "$}'" + "/n");
+      String outData = String("'{$temp$:$" + tempStr + "$, $depth$:$" + depthStr + "$, $n12$:$" + n12Str + "$, $p12$:$" + p12Str + "$}'python" + "/n");
 
       Serial.print(outData);
 
